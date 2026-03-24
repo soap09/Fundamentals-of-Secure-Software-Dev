49af9f2e096f12b7879c6e8e6799eada45534793
soap09 <a8silence@gmail.com>
2026-03-24 13:47:13 +0700

    Fixed main exec prog

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #define MAX_ATTEMPTS 3
 #define MAX_LEN 50
 
-void res(void);  
-void pec(void);
-void ctr(void);
+int res(void);  
+int pec(void);
+int ctr(void);
 
 int main() {
     char login[MAX_LEN];
@@ -48,61 +48,52 @@ int main() {
         attempts++;
         printf("\nПопытка %d из %d:\n", attempts, MAX_ATTEMPTS);
 
-        // Ввод логина
         printf("Введите логин: ");
-        scanf("%49s", login); // Ограничиваем ввод до 49 символов + \0
+        scanf("%49s", login);
 
-        // Ввод пароля
         printf("Введите пароль: ");
-        scanf("%49s", password); // Ограничиваем ввод до 49 символов + \0
+        scanf("%49s", password);
 
-        // Проверка правильности логина и пароля
         if (strcmp(login, CORRECT_LOGIN) == 0 && strcmp(password, CORRECT_PASSWORD) == 0) {
             login_success = 1;
-            break; // Выходим из цикла при успешном входе
+            break;
         } else {
             printf("Неверный логин или пароль\n");
         }
     }
 
-    // Анализ результата
     if (login_success) {
         printf("\nВход выполнен! Добро пожаловать в систему!\n");
         
-        
-    int choice;
-    while (1) {
+        int choice;
+        while (1) {
+            printf("\nВыберите программу для запуска:\n");
+            printf("1 — Программа №1 (пересечение треугольника с кругом)\n");
+            printf("2 — Программа №2 (системы счисления)\n");
+            printf("3 — Программа №3 (римские числа)\n");
+            printf("0 — Выход\n");
+            printf("Введите номер программы: ");
 
-    
-    printf("Выберите программу для запуска:\n");
-    printf("1 — Программа №1\n");
-    printf("2 — Программа №2\n");
-    printf("3 — Программа №3\n");
-    printf("0 — Выход\n");
-    printf("Введите номер программы: ");
+            scanf("%d", &choice);
 
-    scanf("%d", &choice);
-
-    switch (choice) {
-        case 1:
-            res();
-            break;
-        case 2:
-            pec();
-            break;
-        case 3:
-            ctr();
-            break;
-        case 0:
-            printf("Выход из программы.\n");
-            return 0;
-        default:
-            printf("Неверный выбор!\n");
-    }
-
-    }
-    } 
-    else {
+            switch (choice) {
+                case 1:
+                    res();
+                    break;
+                case 2:
+                    pec();
+                    break;
+                case 3:
+                    ctr();
+                    break;
+                case 0:
+                    printf("Выход из программы.\n");
+                    return 0;
+                default:
+                    printf("Неверный выбор!\n");
+            }
+        }
+    } else {
         printf("\nПревышено количество попыток. Программа завершается.\n");
     }
 
